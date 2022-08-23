3632f59ba5b95e785687c3569d61e22590badd5d
Javi <javivillarreal@23branches.com>
2022-08-23 17:16:55 -0500

    added camera functionality

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -10,6 +11,14 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   // ignore: prefer_final_fields
   var _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxHeight: 600,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +50,7 @@ class _ImageInputState extends State<ImageInput> {
             icon: Icon(Icons.camera),
             label: Text('Take Picture'),
             textColor: Theme.of(context).primaryColor,
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
